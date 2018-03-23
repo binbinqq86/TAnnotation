f20d298ce7d9d246d5379ced4ba353f8a4b65aa3
binbinqq86 <624715451@qq.com>
2018-03-23 16:43:57 +0800

    update

--- app/src/main/java/example/tb/com/tannotation/TestActivity.java
+++ app/src/main/java/example/tb/com/tannotation/TestActivity.java
@@ -3,6 +3,7 @@ package example.tb.com.tannotation;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 /**
  * @auther tb
@@ -10,6 +11,9 @@ import android.support.annotation.Nullable;
  * @desc
  */
 public class TestActivity extends Activity {
+    
+    TextView tv;
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
